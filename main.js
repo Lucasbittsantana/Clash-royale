a04a5710d00d34a5053506d6751bf5e962f3629e
Lucasbittsantana <lucas.bittencourt.santana@escola.pr.gov.br>
2025-10-29 10:24:22 -0300

    hh

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("Teste")
+avanca =document.querySelectorAll('.btn-proximo')
+console.log(avanca)
+
+avanca.forEach(button => {
+    button.addEventlistener('click', function (){
+    const atual = document.querySelectorAll('.ativo')
+    const proximopasso = 'passo-' + this.getattribute('data-proximo')
+
+    atual.classilist.remove('ativo')
+    document.getElementById(proximopasso).classList.add('ativo')
+
+
+
+
+
+
